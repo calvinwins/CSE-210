907e34a4ec74b9ae9c3f4df55182f9a3873885e3
calvinwins <weaver95@byui.edu>
2022-02-16 10:12:24 -0600

    a

---
+++ rfk-incomplete/rfk/game/casting/artifact.py
@@ -0,0 +1,8 @@
+from game.casting.actor import Actor
+
+class Artifact(Actor):
+
+    def __init__(self,):
+        
+
+# TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
